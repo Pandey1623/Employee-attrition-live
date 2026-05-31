c3746ec51d68332dfa64828fce9871444cc603fc
Pandey1623 <163702233+Pandey1623@users.noreply.github.com>
2026-06-01 00:22:42 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -31,7 +31,7 @@ async function predictDemo() {
     };
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/predict', {
+        const response = await fetch('/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData)
