d5724725078df7511e8001c71abf02cfb61eeb72
Pandey1623 <163702233+Pandey1623@users.noreply.github.com>
2026-06-02 00:09:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
-CORS(app) # Isse aapka frontend dashboard backend se connect hoga
+CORS(app) 
 
 # Model aur Features files ko load karna
 try:
@@ -17,7 +17,7 @@ try:
     model = joblib.load(os.path.join(base_path, 'model', 'attrition_model.pkl'))
     expected_features = joblib.load(os.path.join(base_path, 'model', 'features.pkl'))
     print("====================================")
-    print("🔥 ENGINE START: ML Model Successfully Loaded! 🔥")
+    print(" ENGINE START: ML Model Successfully Loaded! ")
     print("====================================")
 except Exception as e:
     print("====================================")
@@ -32,34 +32,31 @@ def predict():
     try:
         data = request.get_json()
         
-        # Frontend (script.js) se jo data aa raha hai, use model ke variables se map karna
+        #  coming data from Frontend (script.js) , map of modal's variabldata
         ui_data = {
             "Age": int(data.get("Age", 0)),
             "MonthlyIncome": float(data.get("MonthlyIncome", 0)),
             "JobSatisfaction": int(data.get("JobSatisfaction", 1)),
-            "TotalWorkingYears": int(data.get("TotalWorkingYears", 0)),
-            "Overtime_Yes": int(data.get("Overtime_ yes", 0)) # Yahan frontend aur backend ko jodd diya hai
+            "TotalWorkingYears": int(data.get("TotalExperience", 0)),
+            "Overtime_Yes": 1 if data.get("Overtime")== "yes" else 0)) # add here frontend and backend 
         }
         
-        # Features ki list banana machine learning model ke sequence ke hisaab se
+        # make Features  list acording to machine learning model's sequence 
         features_list = []
         for feature in expected_features:
             matched_value = 0 # Safe default value
             for key, val in ui_data.items():
-                # Dono ke name match karna (agar spaces ya capital letter ka farq ho toh bhi handle ho jaye)
                 if key.lower().replace("_", "").strip() == feature.lower().replace("_", "").strip():
                     matched_value = val
                     break
             features_list.append(matched_value)
 
-        # Data ko 2D array mein badalna jo model ko chahiye
         final_features = np.array(features_list).reshape(1, -1)
 
         # Prediction lena (1 = Chala jayega, 0 = Rukega)
         prediction = model.predict(final_features)
         result = int(prediction[0])
-        
-        # Frontend ko ekdum saaf data bhejna taaki dashboard update ho sake
+    
         return jsonify({
             "status": "success",
             "attrition_prediction": result,
