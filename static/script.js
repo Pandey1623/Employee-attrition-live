88f7a86b61808d56fdcf7c69185d36754ff1f940
Pandey1623 <163702233+Pandey1623@users.noreply.github.com>
2026-06-04 12:28:24 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -48,6 +48,7 @@ async function predictDemo() {
     } catch (e) { 
             // Alert hata diya hai taaki popup na aaye
             console.log("Backend connection skipped for UI stability.");
+          showPage('prediction', document.querySelectorAll(".menu a")[2]);
          }
 }
 
